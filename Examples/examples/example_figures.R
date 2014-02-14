6a922e10b5669b0fcd7b465fa833120b30022f27
Jacob Cecile <jacobcecile@gmail.com>
2014-02-13 20:06:01 -0500

    Yamal and Tornetrask example figures complete.

--- Examples/examples/example_figures.R
+++ Examples/examples/example_figures.R
@@ -1,10 +1,10 @@
+# Libraries ####
+library(dplR)
+library(ggplot2)
+
 # Load data from each example ####
 yamal_effects <- read.csv(file="./Examples/yam/yml_effects.csv", header=TRUE)
 torn_effects <- read.csv(file="./Examples/torn/trn_effects.csv", header=TRUE)
-
-# Don't load in rownames as a new colummn
-yamal_effects <- yamal_effects[,2:ncol(yamal_effects)]
-torn_effects <- torn_effects[,2:ncol(torn_effects)]
 
 yamal_trw_path <- "./Examples/yam/yml-all.raw"
 torn_trw_path <- "./Examples/torn/alltrw.raw"
@@ -45,9 +45,9 @@ birth_years_series_YAMAL <- function(series_name)
 
 birth_years_series_TORN <- function(series_name)
 {
-  if (series_name %in% yamal_po$ID)
+  if (series_name %in% torn_po$ID)
   {
-    position <- which(yamal_po$ID==series_name)
+    position <- which(torn_po$ID==series_name)
     birth_year <- as.numeric(as.character(torn_po$BirthYear[position]))
     return(birth_year)
   } 
@@ -57,27 +57,41 @@ birth_years_series_TORN <- function(series_name)
   }
 }
 
-yamal_birth_years <- sapply(names(yamal_trw), birth_years_series_YAMAL)
-torn_birth_years <- sapply(names(torn_trw),birth_years_series_TORN)
+yamal_birth_years <- unlist(sapply(names(yamal_trw), birth_years_series_YAMAL))
+yamal_birth_years <- data.frame(id=names(yamal_birth_years), birth_year=yamal_birth_years)
+torn_birth_years <- unlist(sapply(names(torn_trw), birth_years_series_TORN))
+torn_birth_years <- data.frame(id=names(torn_birth_years), birth_year=torn_birth_years)
 
-match_birth_year <- function(r, birth_years, df){
-  id <- df[r, "id"]
-  birth_year <- birth_years[names(birth_years)==id]
-  return(birth_year)
-}
+yamal_I_year_df <- merge(yamal_I_year_df, yamal_birth_years, by="id")
+torn_I_year_df <- merge(torn_I_year_df, torn_birth_years, by="id")
 
-yamal_I_year_df$birth_year <- sapply(1:nrow(yamal_I_year_df), match_birth_year, birth_years=yamal_birth_years, df=yamal_I_year_df)
-torn_I_year_df$birth_year <- sapply(1:nrow(torn_I_year_df), match_birth_year, birth_years=torn_birth_years, df=torn_I_year_df)
-
+# Combining data
 yamal_I_year_df$Chronology <- "Yamal"
 torn_I_year_df$Chronology <- "Tornetrask"
 I_year_df <- rbind(yamal_I_year_df, torn_I_year_df)
 
 
-yamal_I_birth_plot <- ggplot(I_year_df, aes(x=birth_year, y=effect)) + geom_point() + geom_smooth(colour="red") + ylab("Individual effect") + xlab("Year of birth") + theme_bw()
+I_birth_plot <- ggplot(I_year_df, aes(x=birth_year, y=effect)) + geom_point() + geom_smooth(colour="red") + ylab("Individual effect") + xlab("Year of birth") + theme_bw() + facet_grid(Chronology~.)
 print(yamal_I_birth_plot)
 
-
 # Plot time signals ####
+yamal_T$id <- as.numeric(as.character(yamal_T$id))
+torn_T$id <- as.numeric(as.character(torn_T$id))
+
+yamal_T$Chronology <- "Yamal"
+torn_T$Chronology <- "Tornetrask"
+
+joint_T <- rbind(yamal_T, torn_T)
+T_plot <- ggplot(joint_T, aes(y=effect, x=id, colour=model)) + geom_line() + theme_bw() + geom_hline(y=1) + ylab("Time effect") + xlab("Year") + theme_bw() + scale_colour_manual(values=c("black", "red"), name="Model") + facet_grid(Chronology~.) + ylim (c(0, 3))
+print(T_plot) 
 
 # Plot MSB effect ####
+joint_T_TA <- joint_T[joint_T$model=="TA",]
+joint_T_ITA <- joint_T[joint_T$model=="ITA",]
+
+msb_effect <- joint_T_TA
+msb_effect$effect <- joint_T_TA$effect / joint_T_ITA$effect
+
+msb_effect_plot <- ggplot(msb_effect, aes(y=effect, x=id)) + geom_line() + geom_hline(y=1) + ylab("Ratio between uncorrected and corrected time effect") + xlab("Year") + theme_bw() + facet_grid(Chronology~.)
+print(msb_effect_plot)
+
